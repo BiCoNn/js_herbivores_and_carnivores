bf1bd3cd51f2e4b092029c59b2d6ef308fc2584e
Nazar <nazar.shevchuk.121@gmail.com>
2025-06-26 18:26:03 +0300

    sol1

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,46 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
+
+  minusHealth(delta) {
+    this.health += delta;
+
+    if (this.health <= 0) {
+      const i = Animal.alive.indexOf(this);
+
+      Animal.alive.splice(i, 1);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    if (!(target instanceof Herbivore)) {
+      return;
+    }
+
+    if (target.hidden) {
+      return;
+    }
+    target.minusHealth(-50);
+  }
 }
 
 module.exports = {
